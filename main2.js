33d13bc13a18a64cedb9e5bfdb20116d2599f82d
Karla Morfin <karlafmorfin@gmail.com>
2025-10-07 16:22:18 -0600

    commit bucle more changes

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
  const b = "cambios 7 de octubre para restaurar los cambios  - cuando dos personas hacen cambios en el mismo fichero pero en diferentes ramas y el otro ya hizo pull request"
 
  while(true){
-    console.log("bucle infinito")
+    console.log("bucle infinito");
  }
 
 console.log(a);
