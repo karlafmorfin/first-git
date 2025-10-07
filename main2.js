9749250663aea33d1676ffff760ac14bea6fe49e
Karla Morfin <karlafmorfin@gmail.com>
2025-10-07 14:06:44 -0600

    commit nuevo2

--- main2.js
+++ main2.js
@@ -1,3 +1,3 @@
- const b = "cambios 7 de octubre  - cuando dos personas hacen cambios en el mismo fichero pero en diferentes ramas y el otro ya hizo pull request"
+ const b = "cambios 7 de octubre para git stash  - cuando dos personas hacen cambios en el mismo fichero pero en diferentes ramas y el otro ya hizo pull request"
 
 console.log(a);
